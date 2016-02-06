78f36ebd64fc2c04de762a0165de50a1e7f4cc63
Kyle Owsen <owsenk@uw.edu>
2016-02-06 01:56:38 -0800

    Deliberately failed one Jasmine test for Phonegap Build

--- client/www/js/NotificationManager.js
+++ client/www/js/NotificationManager.js
@@ -1,9 +1,9 @@
 var NotificationManager = function(getData, setDataListener) {
 	
-	setDataListener(["budget", "options"], function(type) {
-		// get options and budget, destroy and remake notifications
-		console.log("TYPE: " + type)
-	});	
+	// setDataListener(["budget", "options"], function(type) {
+	// 	// get options and budget, destroy and remake notifications
+	// 	console.log("TYPE: " + type)
+	// });	
 
 	setDataListener("ready", function() {
 		// set initial notifications

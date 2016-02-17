fbb8449bc9f700e5c896c291cdfe913ae5a9991a
git44 <lizs3434@gmail.com>
2016-02-17 14:15:33 -0800

    started Options

--- client/www/js/UIView.js
+++ client/www/js/UIView.js
@@ -38,18 +38,6 @@ var UIView = function(getData, setDataListener) {
 		});
 	});
 	
-	// //delete items from savings list
-	// $(function() {
-	// 	$("#savingsList").UIEditList({
-	// 		editLabel: "DELETE ENTRY",
-	// 		movable: false,
-	// 		deletable: true,
-	// 		callback: function(list) {
-	// 			//TODO:callback
-	// 		}
-	// 	});
- //    });
-	
 	//append to savings entry list
 	function appendSavingsList(ctx) {
 		$("#savingsList").append('<div><li id ="'+ ctx.name + '"><h3>' + ctx.name + '</h3><h3 id="prev' + ctx.name + '">$' + ctx.amount +'</h3><input id="text' + ctx.name + '" data-controller="input-value" type="number" min = "0"><button class="ui-btn ui-btn-inline" id="button' + ctx.name + '" >Update</button><p id="save' + ctx.name +'"></p></li></div>');
@@ -84,13 +72,6 @@ var UIView = function(getData, setDataListener) {
 	});
 	
 	//-----------------BUTTONS--------------------------
-	
-	/*//hide savings edit buttons on return to main page
-	$("#saveNext").click(function() {
-		$('#savingsList').removeClass('showIndicators');
-        $('button.done').addClass('edit').removeClass('done').text('Edit');
-		//$('button.delete').hide();
-	});*/
 	
 	//make new element
 	function makeTemplate(catName, updateFn, listId) {
@@ -169,37 +150,6 @@ var UIView = function(getData, setDataListener) {
 		}]);
 		//Todo: add uuid as name and val to entry
 	}
-		//hide delete
-		//$('#savingsList').removeClass('showIndicators');
-        //$('button.done').addClass('edit').removeClass('done').text('Edit');
-		//$('button.delete').hide();
-		
-        /*$.UIPopup({
-			id: "addEntrySavings",
-			title: 'Input Entry Name', 
-			cancelButton: 'CANCEL', 
-			continueButton: 'OK', 
-			message: '<input id="entryBox" data-controller="input-value">',
-			callback: function() {
-				//BUGGY: THIS LINE IS CURRENTLY RETURNING UNDEFINED for #entryBox
-				var temp = new SavingsEntry($("#entryBox").text(), 0, false);
-				
-				notifyListeners("addEntry", ["savings", temp, function() {
-					var popupMessageTarget = document.querySelector('#popupMessageTarget');
-					popupMessageTarget.textContent = 'Entry added.';
-					popupMessageTarget.classList.remove("animatePopupMessage");
-					popupMessageTarget.classList.add("animatePopupMessage");
-					
-					appendSavingsList(temp);
-				}, function(message) {
-					var popupMessageTarget = document.querySelector('#popupMessageTarget');
-					popupMessageTarget.textContent = 'Entry add FAILED.' + temp.name;
-					popupMessageTarget.classList.remove("animatePopupMessage");
-					popupMessageTarget.classList.add("animatePopupMessage");
-				}]);
-			}
-		});
-    });*/
 
 	$("#addCharge").click(function() {
 		var lastLi =  $("#chargesList")[$("#chargesList").length -1];

2b026c783135c3cc2962fcf59138f5a4a4696126
Kyle Owsen <owsenk@uw.edu>
2016-02-20 17:14:30 -0800

    Added very rough notification setting, untested.

--- client/www/js/NotificationManager.js
+++ client/www/js/NotificationManager.js
@@ -8,10 +8,52 @@ var NotificationManager = function(getData, setDataListener) {
 		cordova.plugins.notification.local.clearAll(function() {
 			var options = getData('options');
 			var budget = getData('budget');
+			var time;
+
 			// set morning at time if isNotifyMorning
+			if(options.isNotifyMorning) {
+				time = new Date();
+				time.setDate(time.getDate() + 1);
+				time = copyTimeOfDay(time, new Date(options.notifyMorningTime));
+				setNotification("Your budget is $" + budget, time);
+			}
+
 			// set night at time if isNotifyNight
+			if(options.isNotifyNight) {
+				time = new Date();
+				time = copyTimeOfDay(time, new Date(options.notifyNightTime));
+				setNotification("Tap here to track your spending", time);
+			}
+
 			// set assets at time and date if isNotifyAssets
+			if(options.isNotifyAssets) {
+				time = new Date();
+				time.setDate(1);
+				if(time < (new Date())) {
+					time.setMonth(time.getMonth() + 1);
+				}
+				time.setHour(9);
+				time.setMinutes(0);
+				setNotification("Remember to check your assets!", time);
+			}
+
 		});
 	});
+
+	function copyTimeOfDay(destDate, sourceDate) {
+		destDate.setHour(sourceDate.getHour());
+		destDate.setMinutes(sourceDate.getMinutes());
+		destDate.setSeconds(sourceDate.getSeconds());
+		return destDate;
+	}
+
+	function setNotification(text, time) {
+		cordova.plugins.notification.local.schedule({
+		    text: text,
+		    at: time,
+		    led: "FF0000",
+		    sound: null
+		});
+	}
 
 };

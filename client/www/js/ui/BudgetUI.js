0d1ff9bcf076eb8b7f6ab9f3ed273a46dd71862e
Kyle Owsen <owsenk@uw.edu>
2016-03-07 15:06:48 -0800

    Budget font now scales based on length

--- client/www/js/ui/BudgetUI.js
+++ client/www/js/ui/BudgetUI.js
@@ -1,8 +1,17 @@
 function BudgetUI(getData, setDataListener) {
 
 	function fetchBudget() {
-		$("#budget").html("$" + getData("budget"));
+		var budgetString = "$" + getData("budget");
+		$("#budget").html(budgetString);
+
+		var length = Math.max(budgetString.length, 3);
+
+		var size = (2400 / length) + "%";
+		$("#budget").css('font-size', size);
 	}
+
+// #budget {
+// 	font-size: 1000%;
 
 	setDataListener("budget", fetchBudget);
 
